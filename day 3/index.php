3d85342ae9685f68f2d1a42069f5cc54a4b95ee1
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-03-14 08:10:08 +0545

    Add initial HTML and PHP files for day 3 exercises

---
+++ day 3/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+
+/*
+- Installation of xampp and php
+- Introduction to PHP
+- Running the first PHP script
+- Basic syntax of PHP
+- Case sensitivity in PHP
+- Comments in PHP
+*/
+
+ECHO 'My first PHP script!<br>';
+// Checking the PHP version
+ECHo 'PHP version: ' . PHPVERSION();
+
+# Understanding case sensitivity in PHP
+$bishal = "Bishal";
+$BISHAL = "Rijal";
+echo "<br>";
+echo $bishal;
+echo "<br>";
+echo $BISHAL;
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line
+comment */
+
+
+?>
+<h1>Hello World!</h1>
+
+</body>
+</html>
